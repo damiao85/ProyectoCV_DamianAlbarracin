909fcf0c1f2f3df3688e6272cffef34a787009b0
Damian Albarracin <ndalbarracin85@gmail.com>
2023-02-28 23:00:12 -0300

    Primera escritura en el repositorio

---
+++ javascript/function.js
@@ -0,0 +1,13 @@
+document.getElementById('boton').onclick = function() {
+    document.body.style.backgroundColor= 'black';
+    document.body.style.color= 'white';
+
+
+}
+
+document.getElementById('botonDesactivarModoOscuro').onclick = function() {
+    document.body.style.backgroundColor= 'lightgray';
+    document.body.style.color= 'maroon';
+
+
+}
